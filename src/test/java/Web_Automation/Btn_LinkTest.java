e893f6dca7906c197a64c8e86c081475ddb425bd
Amitch.147 <Amitch.147@gmail.com>
2022-02-04 14:36:17 +0530

    adding files

---
+++ src/test/java/Web_Automation/Btn_LinkTest.java
@@ -0,0 +1,18 @@
+package Web_Automation;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Btn_LinkTest {
+
+    public static void main(String args[])
+    {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.findElement(By.linkText("Add/Remove Elements")).click();
+
+    }
+}
